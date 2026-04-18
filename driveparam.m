38cb33ec56cf2f3d10b232a64ba4e481fe7dbfd1
Justin Y <justinyu@bu.edu>
2026-04-17 23:33:06 -0400

    drivecontrol works, imu broken again b/c of non-saved git push

--- driveparam.m
+++ driveparam.m
@@ -32,8 +32,8 @@ Ke = 0.03;           % Back-EMF C   onstant
 Kt = 0.03;           % Torque Constant
 
 % GUI Control Params
-v_ref_cmd  = 0.05;
-w_ref_cmd  = 0;
+v_ref_cmd  = 0.01;
+w_ref_cmd  = 0.8;
 enable_cmd = 1;
 estop_cmd  = 0;
 
